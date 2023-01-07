1b54cd2ea6101220eb1a3e95bb060960a5c0e0c1
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-01-07 23:20:44 +0530

    third commit on 06-01-23

---
+++ JavaScript/asynchronous.js
@@ -0,0 +1,44 @@
+
+function getTodo1(){
+    console.log("before setTimeout");
+
+    //simulating code delay using setTimeout
+
+    setTimeout(function(){
+        var a = 10;
+        var b= 20;
+        console.log("setTimeout called " + (a*b));
+    },1000);
+
+    console.log("after setTimeout");
+
+}
+
+//getTodo1();
+
+//without callback
+function getTodo(){
+    setTimeout(function(){
+        return {text:"Complete data"};
+    },1000);
+}
+
+//let data = getTodo();
+//console.log(data.text);
+
+//getTodo();
+
+//with callback
+
+function getTodoCallback(callback1){
+    setTimeout(function(){
+        var data = {text:"Data complete"};
+        callback1(data);
+    },2000);
+}
+
+function printData(data){
+    console.log(data.text);
+}
+
+getTodoCallback(printData);
