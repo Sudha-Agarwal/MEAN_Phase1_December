1604b6db5582d4d94cb598e93360c7354dccc616
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-01-08 21:37:51 +0530

    third commit on 08-01-23

--- JavaScript/ajax_callback.js
+++ JavaScript/ajax_callback.js
@@ -1,3 +1,21 @@
 function makeAjaxCall(url, methodType, callback){
-    
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(methodType, url);
+    xhr.send();
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            if(xhr.status == 200){      
+                callback(xhr.response);
+            }
+        }
+    }
 }
+
+
+function display(data){
+    document.getElementById("demo").innerHTML = data;
+}
+var URL = "https://pokeapi.co/api/v2/pokemon?limit=50";
+makeAjaxCall(URL, "GET", display);
